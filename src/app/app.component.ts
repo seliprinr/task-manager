31e52f5a2328b03aa54bfab9f9bae96b19e26efc
Rodrigo Seliprin <rseliprin@gmail.com>
2024-09-13 15:01:30 -0300

    Exercício em Angular para o Curso de TI da UFSM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgIf, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] 
 })
 export class AppComponent {
   title = 'task-manager';
+
+  listaTarefas: string[] = ['Estudar Angular', 'Praticar Exercícios', 'Pagar boletos'];
 }
